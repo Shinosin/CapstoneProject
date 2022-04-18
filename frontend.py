51d02c792e91df523b55ecca3268b42461cd4692
HO WEN XUAN WEN <98514324+wenxuan2128@users.noreply.github.com>
2022-04-18 05:21:58 +0000

    1) added all the block title thingies shin told me to add 2) data type and return type in main

--- frontend.py
+++ frontend.py
@@ -7,19 +7,48 @@ def index(message=''):
     return render_template('index.html',
                            message=message)
 
-def redirect(data):
+def redirect(data:dict) -> None:
     return render_template('redirect.html',
                           form_data=data)
 
-#student and class
-def view_student(data):
+#student
+'''
+A form for students to view existing students.
+
+Arguments:
+- cca_id: int
+- cca_name: str
+'''
+def view_student(data:dict) -> None:
     # display data
     return render_template('view.html',
                            entity="student",
                           data=data)
 
+#class
+'''
+A form for students to view existing classes.
+
+Arguments:
+- class_id: int
+- class_name: str
+- class_level: str
+'''
+def view_class(data:dict) -> None:
+    # display data
+    return render_template('view.html',
+                           entity="class",
+                          data=data)
+
 #cca
-def add_cca():
+'''
+A form for students to add and view a cca.
+
+Arguments:
+- cca_id: int
+- cca_name: str
+'''
+def add_cca() -> None:
     return render_template(
         "add_cca.html",
         page_type="new",
@@ -31,7 +60,7 @@ def add_cca():
             "cca_name": ""
         })
     
-def confirm_cca(cca_name):
+def confirm_cca(cca_name:str) -> None:
     return render_template(
         "add_cca.html",
         page_type="confirm",
@@ -43,13 +72,22 @@ def confirm_cca(cca_name):
             "cca_name": cca_name 
         })
 
-def view_cca(data):
+def view_cca(data:dict) -> None:
     return render_template('view.html',
                            entity="cca",
                           data=data)
 
 #activity
-def add_activity():
+'''
+A form for students to add an activity.
+
+Arguments:
+- activity_id: int
+- activity_startdate: int
+- activity_enddate: int
+- activity_desc: str
+'''
+def add_activity() -> None:
     return render_template(
             "add_activity.html",
             page_type="new",
@@ -62,7 +100,7 @@ def add_activity():
                 "activity_desc": ""
             })
 
-def confirm_activity(data):
+def confirm_activity(data:dict) -> None:
     return render_template(
         "add_activity.html",
         page_type="confirm",
@@ -73,7 +111,7 @@ def confirm_activity(data):
         form_data=data
     )
 
-def view_activity(data):
+def view_activity(data:dict) -> None:
     return render_template('view.html',
                            entity="activity",
                           data=data)    
@@ -83,6 +121,7 @@ def view_activity(data):
 A form for students to enter a student name and cca name, to add a student into a cca.
 
 Arguments:
+- student_id: int
 - student_name: str
 - cca_name: str (dropdown)
 '''
@@ -93,6 +132,7 @@ Arguments:
 A form for students to enter a student name and activity name, to add a student into an activity
 
 Arguments:
+- student_id: int
 - student_name: str
 - activity_name: str (dropdown)
 '''
