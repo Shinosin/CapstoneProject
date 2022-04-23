37cdd986932ff39eb338eacdf8082c1b30162899
SHIN LENG QUEK <85054572+Shinosin@users.noreply.github.com>
2022-04-23 05:24:30 +0000

    finished add_cca and add_activity

--- main.py
+++ main.py
@@ -13,32 +13,57 @@ def splash():
 
 @app.route('/index', methods=['POST','GET'])
 def index():
-    """Index Page. User actions are displayed
+    """Index Page. User actions are displayed.
     Access to: /add_cca, /add_activity, /profile,
                /view_student, /view_class, /view_cca, /view_activity
-               /edit_membership, /edit_participation
+               /edit_membership, /edit_participation.
     """
     return frontend.index()
 
 @app.route('/add_cca', methods=['POST', 'GET'])
 def add_cca():
+    """Creating a new club
+    1. New Form for students to fill in new club data (club name)
+    2. Confirmation page for students to ensure the details are correct
+    3. Verify with database if Club name already exists
+    """
     if "confirm" in request.args: # confirmation page
-        cca_name = request.form["cca_name"]
-        return frontend.confirm_cca(cca_name)
+        cca_name = request.form["cca_name"] # str
+        return frontend.confirm_cca(cca_name) # cca_name: str
+    elif 'verify' in request.args: # check with database
+        cca_name = request.form.to_dict() # dict
+        if storage.database['cca'].insert(cca_name): # successfully inserted
+            return frontend.redirect(cca_name) # cca_name: dict
+        else:
+            return frontend.add_cca(message='Failed to add CCA, CCA already exists.')
     else:
         return frontend.add_cca()
 
 @app.route('/add_activity', methods=['POST', 'GET'])
 def add_activity():
+    """Creating a new activity
+    1. New Form for students to fill in new activity data
+    2. Confirmation page for students to ensure the details are correct
+    3. Verify with database if Activity name already exists
+    """
     if 'confirm' in request.args:
-        data = dict(request.form)
+        data = request.form.to_dict() # dict
         return frontend.confirm_activity(data)
+    elif 'verify' in request.args:
+        data = request.form.to_dict() # dict
+        if storage.database['activity'].insert(data): # successfully inserted
+            return frontend.redirect(data) # data: dict
+        else:
+            return frontend.add_activity(message='Failed to add Activity, Activity already exists.')
+            
     else:
         return frontend.add_activity()
 
 @app.route('/view_student', methods=['POST', 'GET'])
 def view_student():
-    return frontend.view_student()
+    if request.method == 'POST':
+        data = storage.database['students'].get_all()
+        return frontend.view_student(data)
     # I WANT DATA SHIN IN DICTIONARY FORM THANK
 
 @app.route('/view_class', methods=['POST', 'GET'])
